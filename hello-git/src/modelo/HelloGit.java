360f9aeb38145cb0aab2c32f573af4cc324a5a36
Leonardo Passoni <leonardo.passoni@sgsistemas.com.br>
2022-09-02 10:05:27 -0300

    Commit na OS-123456

--- hello-git/src/modelo/HelloGit.java
+++ hello-git/src/modelo/HelloGit.java
@@ -3,7 +3,7 @@ package modelo;
 public class HelloGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git");
-        System.out.println("Hello");
+        System.out.println("Primeiro commit na OS-123456");
+
     }
 }
